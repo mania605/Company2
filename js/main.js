b3fb41a224346a368af005e9410266b9b530ca07
mania605 <kim.minsun83@gmail.com>
2024-09-30 11:19:16 +0900

    스크롤러 수정

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ import Anime from "./anime.js";
 
 const h1 = document.querySelector("header h1");
 
-h1.addEventListener("click", e => {
-  e.preventDefault();
-  new Anime(window, { scroll: 700 }, { duration: 1000 });
-});
+const scroll_btns = ducument.querySelectorAll('.scroller li');
+
+scroll_btns.forEach(btn => {
+  btns.addEventListener("click", e => {
+  });
+
