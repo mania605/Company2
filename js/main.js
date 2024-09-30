90c9eeeee12008a40b1bb691d11b29408d4f8bf5
mania605 <kim.minsun83@gmail.com>
2024-09-30 09:16:02 +0900

    company Anime

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import Anime from "./anime.js";
+
+const h1 = document.querySelector("header h1");
+
+h1.addEventListener("click", () => {
+  e.preventDefault();
+  new Anime(window, { scroll: 700 }, { duration: 1000 });
+})
