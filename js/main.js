56fb14c8fb8b6598e44d330bac00ed141a14dec7
mania605 <kim.minsun83@gmail.com>
2024-09-30 15:26:49 +0900

    코드정리2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 import Anime from "./anime.js";
 
 //variables
-let posArr = [];
-const secArr = document.querySelector("main").children;
-const scroll_btns = document.querySelectorAll(".scroller li");
+let posArr = []; //// 각 섹션의 위치값(위로부터 얼마나 떨어져 있는지)을 저장할 배열
+const secArr = document.querySelector("main").children; //// main태그자식 요소들(섹션들)을 선택하여 배열처럼 저장
+const scroll_btns = document.querySelectorAll(".scroller li");//.scroller 클래스 내부의 li 요소들(스크롤 버튼들)을 선택하여 배열처럼 저장
 
 //init (get section position array)
-for (let sec of secArr) posArr.push(sec.offsetTop);
+for (let sec of secArr) posArr.push(sec.offsetTop);////for (let sec of secArr): 각 섹션의 offsetTop 값을 posArr 배열에 추가
 
 //scroll btn evnet
-scroll_btns.forEach((btn, idx) => {
+scroll_btns.forEach((btn, idx) => {//// 각 스크롤 버튼에 클릭 이벤트를 추가
   btn.addEventListener("click", () => {
-    new Anime(window, { scroll: posArr[idx] });
+    new Anime(window, { scroll: posArr[idx] });////Anime 객체를 이용해 클릭한 버튼에 해당하는 섹션의 위치(posArr[idx])로 부드럽게 스크롤 이동하는 애니메이션을 실행
   });
 });
 
 //window scroll event
 window.addEventListener("scroll", () => {
+  const scroll = window.scrollY;
   posArr.forEach((pos, idx) => {
     if (window.scrollY >= pos) [scroll_btns, secArr].forEach(arr => activation(arr, idx));
   });
@@ -27,7 +28,6 @@ function activation(arrEl, index) {
   for (const el of arrEl) el.classList.remove("on");
   arrEl[index].classList.add("on");
 }
-
 
 /*
 문제점1 -특정 위치로 스크롤 이동 후 브라우저 리사이즈 시 버튼(박스) 활성화 위치 어긋남 문제
