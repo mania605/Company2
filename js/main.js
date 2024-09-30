63cb6bc604dc11375c5a9b9a5066288e57f9d2c9
mania605 <kim.minsun83@gmail.com>
2024-09-30 10:55:21 +0900

    메인 amine연결,비주얼영역배경채우기,메인비주얼슬로건스타일링,메인비주얼반응형

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import Anime from "./anime.js";
 
 const h1 = document.querySelector("header h1");
 
-h1.addEventListener("click", () => {
+h1.addEventListener("click", e => {
   e.preventDefault();
   new Anime(window, { scroll: 700 }, { duration: 1000 });
-})
+});
